dbe9c1c4016226f3579950518d8df0efc5e5dfb3
Roman-code-web <selenerg.275@gmail.com>
2022-12-28 12:28:48 -0500

    creacion

---
+++ src/app/modulos/pages/pages.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+
+
+
+@NgModule({
+  declarations: [
+    NosotrosComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PagesModule { }
